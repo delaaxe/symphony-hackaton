cb92f360943e7e83ef46ab3bafb501aa8def7ad5
Elvis <leung251v2@gmail.com>
2018-06-05 10:40:22 +0800

    with iC form template

---
+++ form_template.js
@@ -0,0 +1,39 @@
+module.exports.form =
+[
+    {
+        'name': 'subject',
+        'type': 'String',
+        'value': null,
+        'required': true,
+    },
+    {
+        'name': 'account',
+        'type': 'List',
+        'value': [],
+        'required': true,
+    },
+    {
+        'name': 'date',
+        'type': 'Datetime',
+        'value': null,
+        'required': true,
+    },
+    {
+        'name': 'content',
+        'type': 'String',
+        'value': null,
+        'required': true,
+    },
+    {
+        'name': 'sg_participants',
+        'type': 'List',
+        'value': [],
+        'required': true,
+    },
+    {
+        'name': 'account_participants',
+        'type': 'List',
+        'value': [],
+        'required': false,
+    }
+]
--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const Symphony = require('symphony-api-client-node');
 const recastai = require('recastai');
 const client = new recastai.request('512032aa56aa69d2281f16b8644deeae');
+const form = require('./form_template').form;
 var message_queue = [];
 
 const botHearsSomething = ( event, messages ) => {
     messages.forEach( (message, index) => {
       if (message.messageText == '!import'){
         // import the message queue
-        
+        import_form = Object.assign({}, form);
       }else{
         client.converseText(message.messageText)
         .then(function(res) {
