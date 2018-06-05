50fb27b966ebe9bd04c2d9bdc7e14d6c3020db7f
Guillaume Bissinger <guillaume.bissinger@gmail.com>
2018-06-05 10:42:44 +0800

    merge

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const Symphony = require('symphony-api-client-node');
+const Bot = require("./bot_logic")
 
 const botHearsSomething = ( event, messages ) => {
     messages.forEach( (message, index) => {
-      let reply_message = 'Message: ' + JSON.stringify(message) + '\n Event: ' + JSON.stringify(event);
-      console.log(message)
-      console.log(event)
+      let reply_message = 'Message: ' + message.user.firstName  ;
+      bot.read(message)
+      // console.log(message)
+      // console.log(event)
       Symphony.sendMessage( message.stream.streamId, reply_message, null, Symphony.MESSAGEML_FORMAT);
     })
 }
