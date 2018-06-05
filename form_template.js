8aa10a592995e34bd51fafa5225ab0184fd5202e
Elvis <leung251v2@gmail.com>
2018-06-05 13:31:17 +0800

    asking for missing fields

--- form_template.js
+++ form_template.js
@@ -1,39 +1,40 @@
-module.exports.form =
-[
-    {
-        'name': 'subject',
-        'type': 'String',
-        'value': null,
-        'required': true,
-    },
-    {
-        'name': 'account',
-        'type': 'List',
-        'value': [],
-        'required': true,
-    },
-    {
-        'name': 'date',
-        'type': 'Datetime',
-        'value': null,
-        'required': true,
-    },
-    {
-        'name': 'content',
-        'type': 'String',
-        'value': null,
-        'required': true,
-    },
-    {
-        'name': 'sg_participants',
-        'type': 'List',
-        'value': [],
-        'required': true,
-    },
-    {
-        'name': 'account_participants',
-        'type': 'List',
-        'value': [],
-        'required': false,
-    }
-]
+module.exports.template = function() {
+    return [
+        {
+            'name': 'subject',
+            'type': 'String',
+            'value': null,
+            'required': true,
+        },
+        {
+            'name': 'account',
+            'type': 'List',
+            'value': null,
+            'required': true,
+        },
+        {
+            'name': 'date',
+            'type': 'Datetime',
+            'value': null,
+            'required': true,
+        },
+        {
+            'name': 'content',
+            'type': 'String',
+            'value': null,
+            'required': true,
+        },
+        {
+            'name': 'sg_participants',
+            'type': 'List',
+            'value': null,
+            'required': true,
+        },
+        {
+            'name': 'account_participants',
+            'type': 'List',
+            'value': null,
+            'required': false,
+        }
+    ]
+}
--- index.js
+++ index.js
@@ -1,14 +1,62 @@
 const Symphony = require('symphony-api-client-node');
 const recastai = require('recastai');
 const client = new recastai.request('512032aa56aa69d2281f16b8644deeae');
-const form = require('./form_template').form;
+const form = require('./form_template');
+var import_form = null;
 var message_queue = [];
+var getting = null;
+var importing = false;
+
+function getting_missing_fields(message){
+  // subject field
+  if (import_form[0].value === null && import_form[0].required === true){
+    getting = 'subject';
+    console.log('[debug] ask for subject');
+    Symphony.sendMessage( message.stream.streamId, "What is the subject of form?", null, Symphony.MESSAGEML_FORMAT);
+  }
+
+  // account field
+  else if(import_form[1].value === null && import_form[1].required === true){
+    getting = 'account';
+    console.log('[debug] ask for account');
+    Symphony.sendMessage( message.stream.streamId, "What is the account of form?", null, Symphony.MESSAGEML_FORMAT);
+  }
+
+  else{
+    getting = null;
+    console.log('=== import finished ===');
+    console.log('import_form:',import_form);
+    Symphony.sendMessage( message.stream.streamId, "The form is well imported.", null, Symphony.MESSAGEML_FORMAT);
+  }
+}
 
 const botHearsSomething = ( event, messages ) => {
     messages.forEach( (message, index) => {
-      if (message.messageText == '!import'){
+      if (getting != null && getting === 'subject'){
+        console.log('[debug] got subject')
+        import_form[0].value = message.messageText;
+        getting_missing_fields(message);
+      }
+      else if (getting != null && getting === 'account'){
+        console.log('[debug] got account')
+        import_form[1].value = [message.messageText];
+        getting_missing_fields(message);
+      }
+
+      else if (message.messageText == '!import'){
         // import the message queue
-        import_form = Object.assign({}, form);
+        import_form = form.template();
+        message_queue.forEach( (message_nlp) =>{
+          if (message_nlp.nlp.datetime != undefined){
+            var date_field = import_form.find(function(element) {
+              return element.name === 'date';
+            });
+            date_field.value = message_nlp.nlp.datetime[0].formatted;
+          }
+        });
+        getting_missing_fields(message);
+
+        
       }else{
         client.converseText(message.messageText)
         .then(function(res) {
