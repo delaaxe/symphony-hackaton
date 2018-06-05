61d649b2a1c58f380403e6c6df6bff04c7348df2
Guillaume Bissinger <guillaume.bissinger@gmail.com>
2018-06-05 13:59:21 +0800

    idex

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 const Symphony = require('symphony-api-client-node');
 const Bot = require("./bot_logic")
 
-const botHearsSomething = ( event, messages ) => {
-    messages.forEach( (message, index) => {
-      let reply_message = 'Message: ' + message.user.firstName  ;
-      bot.read(message)
-      // console.log(message)
-      // console.log(event)
-      Symphony.sendMessage( message.stream.streamId, reply_message, null, Symphony.MESSAGEML_FORMAT);
-    })
-}
+bt = new Bot()
+bt.run()
 
-Symphony.initBot(__dirname + '/config.json')
-  .then( (symAuth) => {
-    Symphony.getDatafeedEventsService( botHearsSomething );
-  })
+// const botHearsSomething = ( event, messages ) => {
+//     messages.forEach( (message, index) => {
+//       let reply_message = 'Message: ' + message.user.firstName  ;
+//       bot.read(message)
+//       // console.log(message)
+//       // console.log(event)
+//       Symphony.sendMessage( message.stream.streamId, reply_message, null, Symphony.MESSAGEML_FORMAT);
+//     })
+// }
+
