a07917254c353534f80bd9ba8fca3287b5fa402e
Guillaume Bissinger <guillaume.bissinger@gmail.com>
2018-06-05 15:38:28 +0800

    v1

--- form_template.js
+++ form_template.js
@@ -6,41 +6,47 @@ module.exports.template = function() {
             'value': null,
             'required': true,
             'question': 'What was the topic of your discussion ?',
+            'nlp_type': "subject"
         },
-        {
-            'name': 'account',
-            'type': 'List',
-            'value': null,
-            'required': true,
-            'question': 'What is the account ?',
-        },
-        {
-            'name': 'date',
-            'type': 'Datetime',
-            'value': null,
-            'required': true,
-            'question': 'What is the date ?',
-        },
-        {
-            'name': 'content',
-            'type': 'String',
-            'value': null,
-            'required': true,
-            'question': 'What is the content ?',
-        },
-        {
-            'name': 'sg_participants',
-            'type': 'List',
-            'value': null,
-            'required': true,
-            'question': 'Who are the SG participants ?',
-        },
-        {
-            'name': 'account_participants',
-            'type': 'List',
-            'value': null,
-            'required': false,
-            'question': 'Who are the account participants ?',
-        }
+        // {
+        //     'name': 'account',
+        //     'type': 'List',
+        //     'value': null,
+        //     'required': true,
+        //     'question': 'What is the account ?',
+        //     'nlp_type': null
+        // },
+        // {
+        //     'name': 'date',
+        //     'type': 'Datetime',
+        //     'value': null,
+        //     'required': true,
+        //     'question': 'What is the date ?',
+        //     'nlp_type': "datetime"
+        // },
+        // {
+        //     'name': 'content',
+        //     'type': 'String',
+        //     'value': null,
+        //     'required': true,
+        //     'question': 'What is the content ?',
+        //     'nlp_type': null
+        // },
+        // {
+        //     'name': 'sg_participants',
+        //     'type': 'List',
+        //     'value': null,
+        //     'required': true,
+        //     'question': 'Who are the SG participants ?',
+        //     'nlp_type': "person"
+        // },
+        // {
+        //     'name': 'account_participants',
+        //     'type': 'List',
+        //     'value': null,
+        //     'required': false,
+        //     'question': 'Who are the account participants ?',
+        //     'nlp_type': "person"
+        // }
     ]
 }
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var fs = require('fs');
 const Symphony = require('symphony-api-client-node');
 const recastai = require('recastai');
 const client = new recastai.request('512032aa56aa69d2281f16b8644deeae');
@@ -5,6 +6,25 @@ const form = require('./form_template');
 var import_form = null;
 var message_queue = [];
 var getting = null;
+
+function format_output() {
+  var output = []
+  import_form.forEach(field => {
+    output.push({
+      name: field.name,
+      value: field.value
+    })
+  })
+  return output
+}
+
+function save_output() {
+fs.writeFile("./output.json", JSON.stringify(format_output(), null, 4),  function(err) {
+    if(err) {
+        return console.log(err);
+    }
+}); 
+}
 
 function reset() {
   var import_form = null;
@@ -26,7 +46,9 @@ function getting_missing_fields(message){
     getting = null;
     console.log('=== import finished ===');
     console.log('import_form:',import_form);
+    save_output()
     Symphony.sendMessage( message.stream.streamId, "The form is well imported.", null, Symphony.MESSAGEML_FORMAT);
+    Symphony.sendMessage( message.stream.streamId, "test\ntest\n", null, Symphony.MESSAGEML_FORMAT);
     reset()
   }
 }
@@ -36,8 +58,27 @@ const botHearsSomething = ( event, messages ) => {
 
       if(getting != null) {
         index_field_getting = import_form.findIndex(x => x.name==getting);
-        import_form[index_field_getting].value = message.messageText;
-        getting_missing_fields(message);
+        var message_nlp = {};
+        client.converseText(message.messageText)
+        .then(function(res) {
+          message_nlp.original = message;
+          message_nlp.nlp = res.entities;
+          console.log(message_nlp)
+          if(getting == 'date') {
+            import_form[index_field_getting].value = message_nlp.nlp.datetime[0].formatted;          
+          } else if (getting == 'sg_participants') {
+            import_form[index_field_getting].value = message_nlp.nlp.person.map(function(person) {
+              return person.raw
+            }).join(', ');
+            
+          } else {
+            import_form[index_field_getting].value = message.messageText;
+          }
+  
+          getting_missing_fields(message);
+
+        })
+        
       }
 
       else if (message.messageText == '!import'){
@@ -50,6 +91,21 @@ const botHearsSomething = ( event, messages ) => {
             });
             date_field.value = message_nlp.nlp.datetime[0].formatted;
           }
+          if (message_nlp.nlp.person != undefined){
+            var sg_participants_field = import_form.find(function(element) {
+              return element.name === 'sg_participants';
+            });
+            sg_participants_field.value = message_nlp.nlp.person.map(function(person) {
+              return person.raw
+            }).join(', ');
+          }
+          if (message_nlp.nlp.subject != undefined){
+            var subject_field = import_form.find(function(element) {
+              return element.name === 'subject';
+            });
+            subject_field.value = message_nlp.nlp.subject[0].raw
+          }
+
         });
         getting_missing_fields(message);
 
